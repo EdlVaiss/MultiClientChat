86e7e654d05a6b37a15204635647a57359410a0b
Serge Miliuta <vaissedl@gmail.com>
2018-09-25 17:31:43 +0300

    Add files via upload

--- src/com/vaiss/edl/propertiesholder/PropertiesHolder.java
+++ src/com/vaiss/edl/propertiesholder/PropertiesHolder.java
@@ -5,24 +5,21 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
-import com.vaiss.edl.exceptions.PropertiesException;
 
 public class PropertiesHolder {
-	private Properties properties = new Properties();
+	private static Properties properties = new Properties();
 	
-	public PropertiesHolder() throws PropertiesException {
+	static {
 		try {
 			properties.load(new FileReader("config.properties"));
 		} catch (FileNotFoundException e) {
 			System.out.println("Can't find properties file!");
-			throw new PropertiesException();
 		} catch (IOException e) {
 			System.out.println("Something went wrong while reading properties");
-			throw new PropertiesException();
 		}
 	}
 	
-	public Properties getProperties() {
+	public static Properties getProperties() {
 		return properties;
 	}
 }
