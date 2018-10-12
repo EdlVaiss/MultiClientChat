1379b11cfe2bc810012df653028bc003f59135f7
Serge Miliuta <vaissedl@gmail.com>
2018-10-12 17:44:55 +0300

    long message issue fixed

--- src/com/vaiss/edl/crypto/RSACryptor.java
+++ src/com/vaiss/edl/crypto/RSACryptor.java
@@ -5,12 +5,22 @@ import java.security.PublicKey;
 
 import javax.crypto.Cipher;
 
+import org.apache.log4j.Logger;
+
+import com.vaiss.edl.crypto.utils.ByteArraySplitter;
+import com.vaiss.edl.crypto.utils.HungryByteArrayEater;
+import com.vaiss.edl.exceptions.DecriptionException;
+import com.vaiss.edl.exceptions.EncriptionException;
+
 public class RSACryptor implements Cryptor {
+	private static Logger log = Logger.getLogger(RSACryptor.class);
 	private static RSACryptor instance;
 	private PrivateKey myPrivateKey;
 	private PublicKey myPublicKey;
 	private PublicKey partnerPublicKey;
 	private static final String ALGORITHM = "RSA";
+	private static final int MAX_ENC_MESSAGE_LEN = 245;// in bytes, for encription
+	private static final int MAX_DEC_MESSAGE_LEN = 256;// in bytes, for decription
 
 	private RSACryptor() {
 		KeyGen keyGen = new KeyGen();
@@ -46,28 +56,55 @@ public class RSACryptor implements Cryptor {
 	}
 
 	@Override
-	public byte[] encrypt(String message) {
+	public byte[] encrypt(String message) throws EncriptionException {
 		byte[] cipherMessage = null;
+		byte[] initialMessageBytes = message.getBytes();
 		try {
 			final Cipher cipher = Cipher.getInstance(ALGORITHM);
 			cipher.init(Cipher.ENCRYPT_MODE, partnerPublicKey);
-			cipherMessage = cipher.doFinal(message.getBytes());
+			if (initialMessageBytes.length <= MAX_ENC_MESSAGE_LEN) {
+				cipherMessage = cipher.doFinal(initialMessageBytes);
+			} else {
+				ByteArraySplitter splitter = new ByteArraySplitter();
+				splitter.split(initialMessageBytes, MAX_ENC_MESSAGE_LEN);
+				HungryByteArrayEater eater = new HungryByteArrayEater();
+				while (splitter.hasNext()) {
+					byte[] encodedPart = cipher.doFinal(splitter.next());
+					eater.feed(encodedPart);
+				}
+				cipherMessage = eater.scare();
+			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.fatal("Encription process failed");
+			throw new EncriptionException();
 		}
 		return cipherMessage;
 	}
 
 	@Override
-	public String decript(byte[] message) {
+	public String decript(byte[] message) throws DecriptionException {
 		byte[] dectyptedMessage = null;
 		try {
 			final Cipher cipher = Cipher.getInstance(ALGORITHM);
 			cipher.init(Cipher.DECRYPT_MODE, myPrivateKey);
-			dectyptedMessage = cipher.doFinal(message);
+
+			if (message.length <= MAX_DEC_MESSAGE_LEN) {
+				dectyptedMessage = cipher.doFinal(message);
+			} else {
+				ByteArraySplitter splitter = new ByteArraySplitter();
+				splitter.split(message, MAX_DEC_MESSAGE_LEN);
+				HungryByteArrayEater eater = new HungryByteArrayEater();
+				while (splitter.hasNext()) {
+					byte[] encodedPart = cipher.doFinal(splitter.next());
+					eater.feed(encodedPart);
+				}
+				dectyptedMessage = eater.scare();
+			}
 
 		} catch (Exception ex) {
+			log.fatal("Decription process failed");
 			ex.printStackTrace();
+			throw new DecriptionException();
 		}
 		return new String(dectyptedMessage);
 	}
