fd02d0235c76ecc3910ea920d489cf74d51c4993
Serge Miliuta <vaissedl@gmail.com>
2018-10-12 23:51:30 +0300

    Update Cryptor.java

--- src/com/vaiss/edl/crypto/Cryptor.java
+++ src/com/vaiss/edl/crypto/Cryptor.java
@@ -4,8 +4,8 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 
 public interface Cryptor {
-public byte[] encrypt(String message);
-public String decript(byte[] message);
+public byte[] encrypt(String message) throws EncriptionException;
+public String decript(byte[] message) throws EncriptionException;
 public PublicKey getPartnerPublicKey();
 public void setPartnerPublicKey(PublicKey partnerPublicKey);
 public PrivateKey getMyPrivateKey();
